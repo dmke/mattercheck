8aef932cfc224e04fb906793662493766e279f36
Dominik Menke <dom@digineo.de>
2017-07-19 11:07:35 +0200

    tests for package release

---
+++ releases/releases_test.go
@@ -0,0 +1,84 @@
+package releases
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/blang/semver"
+	"github.com/dmke/mattercheck/version"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/xmlpath.v2"
+)
+
+func init() {
+	body, err := ioutil.ReadFile("testdata/upgrade.html")
+	if err != nil {
+		panic(err)
+	}
+
+	buf := bytes.NewBuffer(body)
+	xml, err := xmlpath.ParseHTML(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	get = func() (*xmlpath.Node, error) {
+		return xml, nil
+	}
+}
+
+func TestFindLatestTeamRelease(t *testing.T) {
+	assert := assert.New(t)
+
+	doc, _ := get()
+	r := findLatestRelease(absTeam, doc)
+	assert.False(r.Version.Enterprise)
+
+	expected, _ := semver.Parse("4.0.1")
+	assert.True(r.Version.EQ(expected))
+}
+
+func TestFindLatestEnterpriseRelease(t *testing.T) {
+	assert := assert.New(t)
+
+	doc, _ := get()
+	r := findLatestRelease(absEnt, doc)
+	assert.True(r.Version.Enterprise)
+
+	expected, _ := semver.Parse("4.0.1")
+	assert.True(r.Version.EQ(expected))
+}
+
+func TestUpdateCandidate(t *testing.T) {
+	assert := assert.New(t)
+
+	archive, err := FetchSupported()
+	assert.NoError(err)
+
+	max, _ := semver.Parse("4.0.1")
+
+	for _, expected := range []struct {
+		have    string
+		wantMax bool
+	}{
+		{"2.0.1", true},
+		{"3.1.2", true},
+		{"3.4.0", true},
+		{"4.0.0", true},
+		{"4.0.1", false},
+	} {
+		for _, ent := range []bool{true, false} {
+			have, err := version.Parse(expected.have, ent)
+			assert.NoError(err)
+
+			candidate := archive.UpdateCandidate(have)
+			if expected.wantMax {
+				assert.NotNil(candidate)
+				assert.True(candidate.Version.EQ(max))
+			} else {
+				assert.Nil(candidate)
+			}
+		}
+	}
+}
